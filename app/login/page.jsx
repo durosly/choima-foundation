5dc5bc22656b3ecf3c09951a5328aceca442e587
durosly <slyboydon1@gmail.com>
2023-09-26 19:29:17 +0100

    ✅create login component

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,3 +1,5 @@
+import LoginForm from "./components/form";
+
 function AdminLoginPage() {
 	return (
 		<div className="hero min-h-screen bg-base-200">
@@ -7,31 +9,7 @@ function AdminLoginPage() {
 					<p className="py-6">Only for authorized users</p>
 				</div>
 				<div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-					<form className="card-body">
-						<div className="form-control">
-							<label className="label">
-								<span className="label-text">Email</span>
-							</label>
-							<input
-								type="text"
-								placeholder="email"
-								className="input input-bordered"
-							/>
-						</div>
-						<div className="form-control">
-							<label className="label">
-								<span className="label-text">Password</span>
-							</label>
-							<input
-								type="text"
-								placeholder="password"
-								className="input input-bordered"
-							/>
-						</div>
-						<div className="form-control mt-6">
-							<button className="btn btn-primary">Login</button>
-						</div>
-					</form>
+					<LoginForm />
 				</div>
 			</div>
 		</div>
