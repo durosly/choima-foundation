9f2d770e4dbfa622f47234e9c1ee009b8504dcba
durosly <slyboydon1@gmail.com>
2023-09-27 00:27:04 +0100

    ✅add toast wrapper

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,3 +1,4 @@
+import ToastWrapper from "./components/toastWrapper";
 import "./globals.css";
 
 import { Inter } from "next/font/google";
@@ -12,7 +13,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
 	return (
 		<html lang="en">
-			<body className={inter.className}>{children}</body>
+			<body className={inter.className}>
+				{children}
+				<ToastWrapper />
+			</body>
 		</html>
 	);
 }
