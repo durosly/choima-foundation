e85d76b2052ab82f7750fb985f14cc3208d884b8
durosly <slyboydon1@gmail.com>
2023-09-28 04:52:39 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	images: {
-		domains: ["images.pexels.com"],
+		domains: ["images.pexels.com", "res.cloudinary.com"],
+		deviceSizes: [640, 750, 828, 1080, 1200],
 	},
 };
 
