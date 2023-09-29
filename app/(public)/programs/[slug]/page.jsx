d936811bc084b0abf27e041f2f7006337dc2372f
durosly <slyboydon1@gmail.com>
2023-09-29 19:10:32 +0100

    ✅display program details

--- app/(public)/programs/[slug]/page.jsx
+++ app/(public)/programs/[slug]/page.jsx
@@ -1,47 +1,87 @@
 import Image from "next/image";
 import { LuCalendar, LuClock5, LuMapPin } from "react-icons/lu";
 import TimelineDisplay from "./components/timeline";
+import connectMongo from "@/lib/connectDB";
+import ProgramModel from "@/models/program";
+import convertTo12HourFormat from "@/lib/formatTime";
+import { DateTime } from "luxon";
 
-function ProgramsDetailsPage() {
+export async function generateMetadata({ params: { slug } }) {
+	await connectMongo();
+
+	const program = await ProgramModel.findOne({ slug });
+
+	if (program) {
+		return {
+			title: program.title,
+			description: program.desc,
+			openGraph: {
+				images: [
+					`${process.env.NEXT_PUBLIC_CLOUDINARY_URL}/${program.cover_image}`,
+				],
+			},
+		};
+	}
+}
+
+async function ProgramsDetailsPage({ params: { slug } }) {
+	await connectMongo();
+
+	const program = await ProgramModel.findOne({ slug });
+
+	if (!program) {
+		notFound();
+	}
+
 	return (
 		<>
 			<div className="px-5 sm:px-10">
 				<div className="relative h-44 w-full rounded-xl overflow-hidden">
 					<Image
 						fill
-						src="https://images.pexels.com/photos/1250452/pexels-photo-1250452.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-						alt=""
+						src={`${process.env.NEXT_PUBLIC_CLOUDINARY_URL}/${program.cover_image}`}
+						alt={program.title}
 						className="object-cover"
 					/>
 				</div>
-				<div className="flex-1">
-					<h2 className="text-2xl font-bold">
-						Lorem ipsum dolor sit amet.
-					</h2>
-					<p>
-						Lorem, ipsum dolor sit amet consectetur adipisicing
-						elit. Atque, dolor?. Lorem ipsum dolor sit amet
-						consectetur adipisicing elit. Laborum maxime, dolorum
-						veritatis aut asperiores perspiciatis magni quis non
-						itaque recusandae.
-					</p>
+				<div className="flex-1 mt-5">
+					<h2 className="text-2xl font-bold">{program.title}</h2>
+					<p>{program.desc}</p>
 					<div className="mt-5 text-sm">
 						<div>
 							<p className="flex items-center gap-2 ">
 								<LuCalendar className="inline-block" />{" "}
-								12/Oct/2022 - 15/Oct/2022
+								{DateTime.fromJSDate(
+									program.start_date
+								).toLocaleString(DateTime.DATE_MED)}{" "}
+								{!!program?.end_date && (
+									<>
+										-{" "}
+										{DateTime.fromJSDate(
+											program.end_date
+										).toLocaleString(DateTime.DATE_MED)}
+									</>
+								)}
 							</p>
 						</div>
 						<div>
 							<p className="flex items-center gap-2 ">
-								<LuClock5 className="inline-block" /> 12:00pm -
-								3:00pm
+								<LuClock5 className="inline-block" />{" "}
+								{convertTo12HourFormat(program.start_time)}{" "}
+								{!!program?.end_time && (
+									<>
+										-{" "}
+										{convertTo12HourFormat(
+											program.end_time
+										)}
+									</>
+								)}
 							</p>
 						</div>
 						<div>
 							<p className="flex items-center gap-2 ">
-								<LuMapPin className="inline-block" /> 12 avenue
-								road
+								<LuMapPin className="inline-block" />{" "}
+								{program.location}
 							</p>
 						</div>
 					</div>
@@ -50,7 +90,7 @@ function ProgramsDetailsPage() {
 					<h3 className="text-2xl font-bold text-center mb-3">
 						Activities
 					</h3>
-					<TimelineDisplay />
+					<TimelineDisplay id={program._id} />
 				</div>
 			</div>
 		</>
