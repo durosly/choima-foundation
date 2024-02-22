fece08081ba88a93ab1b9569d198d25779747f2d
durosly <slyboydon1@gmail.com>
2024-02-22 04:30:59 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	images: {
-		domains: ["images.pexels.com", "res.cloudinary.com"],
+		remotePatterns: [
+			{
+				protocol: "https",
+				hostname: "images.pexels.com",
+				port: "",
+				pathname: "**",
+			},
+			{
+				protocol: "https",
+				hostname: "res.cloudinary.com",
+				port: "",
+				pathname: "**",
+			},
+		],
 		deviceSizes: [640, 750, 828, 1080, 1200],
 	},
 };
