d8a299c7462f5f84ab18e8c05d09d41dc6194ef0
durosly <slyboydon1@gmail.com>
2024-02-22 04:46:54 +0100

    react email error solution

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
+const path = require("path");
+const { NormalModuleReplacementPlugin } = require("webpack");
+
 const nextConfig = {
 	images: {
 		remotePatterns: [
@@ -16,6 +19,20 @@ const nextConfig = {
 			},
 		],
 		deviceSizes: [640, 750, 828, 1080, 1200],
+	},
+	webpack: (
+		config,
+		{ buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }
+	) => {
+		config.plugins = config.plugins || [];
+		config.plugins.push(
+			new NormalModuleReplacementPlugin(
+				/email\/render/,
+				path.resolve(__dirname, "./renderEmailFix.js")
+			)
+		);
+		// Important: return the modified config
+		return config;
 	},
 };
 
